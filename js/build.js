c768489b82f40eb06c71375c535ddb6954e5837d
dannycochran <daniel@cochrans.org>
2014-05-13 20:35:26 -0700

    initial commit

---
+++ js/build.js
@@ -0,0 +1,91 @@
+var components = [
+  {
+    "type":"mast",
+    "img": "assets/img/mast.jpg",
+    "title": "Local Warming",
+    "subtitle": "how global change impacts the Bay Area",
+    "caption": ""
+  },
+  {
+    "type":"section",
+    "paragraphs": [
+       { "content": "Bacon ipsum dolor sit amet strip steak pork chop eu nostrud sint esse ham hock. Anim duis jerky, dolore sed do tongue id. Tenderloin tail frankfurter doner bresaola et bacon aliqua officia ut ribeye ut. Consectetur sirloin shank ball tip, drumstick quis labore t-bone eu pariatur rump. Cillum dolore pork, quis drumstick qui tail turducken est pancetta laborum cupidatat kielbasa tenderloin meatloaf. Est ball tip officia, aliquip shankle cillum meatloaf. Short loin pancetta ea culpa pariatur ut kevin." },
+       { "content": "Ullamco nulla proident excepteur, adipisicing in turkey shoulder enim meatloaf dolore consequat strip steak corned beef. Shoulder velit sirloin turducken dolore tongue kevin porchetta meatloaf boudin magna nulla tri-tip bresaola venison. Aute sirloin capicola, swine ut qui shank bresaola in boudin anim aliquip sausage commodo. Qui short ribs velit frankfurter in commodo. Kevin porchetta enim laboris ut fatback rump. Ut esse shoulder, dolore quis sirloin tempor filet mignon laboris anim pork belly beef spare ribs."},
+       { "content": "Bacon ipsum dolor sit amet strip steak pork chop eu nostrud sint esse ham hock. Anim duis jerky, dolore sed do tongue id. Tenderloin tail frankfurter doner bresaola et bacon aliqua officia ut ribeye ut. Consectetur sirloin shank ball tip, drumstick quis labore t-bone eu pariatur rump. Cillum dolore pork, quis drumstick qui tail turducken est pancetta laborum cupidatat kielbasa tenderloin meatloaf. Est ball tip officia, aliquip shankle cillum meatloaf. Short loin pancetta ea culpa pariatur ut kevin." }
+    ]
+  },
+  {
+    "type": "blurb",
+    "paragraphNum": 0,
+    "img": "assets/img/sarto.jpg",
+    "info": "Nicole studies Earth Systems at Stanford University. She is a Lorry Lokey fellow and Head TA for Earth Systems 10."
+  },
+  {
+    "type":"hiddenSection",
+    "content": "Ullamco nulla proident excepteur, adipisicing in turkey shoulder enim meatloaf dolore consequat strip steak corned beef. Shoulder velit sirloin turducken dolore tongue kevin porchetta meatloaf boudin magna nulla tri-tip bresaola venison. Aute sirloin capicola, swine ut qui shank bresaola in boudin anim aliquip sausage commodo. Qui short ribs velit frankfurter in commodo. Kevin porchetta enim laboris ut fatback rump. Ut esse shoulder, dolore quis sirloin tempor filet mignon laboris anim pork belly beef spare ribs.",
+    "substory": "Did you know?",
+    "img": "assets/img/dam.jpg"
+  },
+  {
+    "type":"section",
+    "paragraphs": [
+       { "content": "Bacon ipsum dolor sit amet strip steak pork chop eu nostrud sint esse ham hock. Anim duis jerky, dolore sed do tongue id. Tenderloin tail frankfurter doner bresaola et bacon aliqua officia ut ribeye ut. Consectetur sirloin shank ball tip, drumstick quis labore t-bone eu pariatur rump. Cillum dolore pork, quis drumstick qui tail turducken est pancetta laborum cupidatat kielbasa tenderloin meatloaf. Est ball tip officia, aliquip shankle cillum meatloaf. Short loin pancetta ea culpa pariatur ut kevin." },
+       { "content": "Ullamco nulla proident excepteur, adipisicing in turkey shoulder enim meatloaf dolore consequat strip steak corned beef. Shoulder velit sirloin turducken dolore tongue kevin porchetta meatloaf boudin magna nulla tri-tip bresaola venison. Aute sirloin capicola, swine ut qui shank bresaola in boudin anim aliquip sausage commodo. Qui short ribs velit frankfurter in commodo. Kevin porchetta enim laboris ut fatback rump. Ut esse shoulder, dolore quis sirloin tempor filet mignon laboris anim pork belly beef spare ribs."},
+       { "content": "Bacon ipsum dolor sit amet strip steak pork chop eu nostrud sint esse ham hock. Anim duis jerky, dolore sed do tongue id. Tenderloin tail frankfurter doner bresaola et bacon aliqua officia ut ribeye ut. Consectetur sirloin shank ball tip, drumstick quis labore t-bone eu pariatur rump. Cillum dolore pork, quis drumstick qui tail turducken est pancetta laborum cupidatat kielbasa tenderloin meatloaf. Est ball tip officia, aliquip shankle cillum meatloaf. Short loin pancetta ea culpa pariatur ut kevin." }
+    ]
+  },
+  {
+    "type":"iframe",
+    "substory":"click to show a map of sea level rise and this title is too long!",
+    "url": "http://sealevel.climatecentral.org/surgingseas/place/cities/CA/San_Francisco#center=10/37.6133/-122.1826&surge=2&show=cities"
+  },
+  {
+    "type":"section",
+    "paragraphs": [
+       { "content": "Bacon ipsum dolor sit amet strip steak pork chop eu nostrud sint esse ham hock. Anim duis jerky, dolore sed do tongue id. Tenderloin tail frankfurter doner bresaola et bacon aliqua officia ut ribeye ut. Consectetur sirloin shank ball tip, drumstick quis labore t-bone eu pariatur rump. Cillum dolore pork, quis drumstick qui tail turducken est pancetta laborum cupidatat kielbasa tenderloin meatloaf. Est ball tip officia, aliquip shankle cillum meatloaf. Short loin pancetta ea culpa pariatur ut kevin." },
+       { "content": "Ullamco nulla proident excepteur, adipisicing in turkey shoulder enim meatloaf dolore consequat strip steak corned beef. Shoulder velit sirloin turducken dolore tongue kevin porchetta meatloaf boudin magna nulla tri-tip bresaola venison. Aute sirloin capicola, swine ut qui shank bresaola in boudin anim aliquip sausage commodo. Qui short ribs velit frankfurter in commodo. Kevin porchetta enim laboris ut fatback rump. Ut esse shoulder, dolore quis sirloin tempor filet mignon laboris anim pork belly beef spare ribs."},
+       { "content": "Bacon ipsum dolor sit amet strip steak pork chop eu nostrud sint esse ham hock. Anim duis jerky, dolore sed do tongue id. Tenderloin tail frankfurter doner bresaola et bacon aliqua officia ut ribeye ut. Consectetur sirloin shank ball tip, drumstick quis labore t-bone eu pariatur rump. Cillum dolore pork, quis drumstick qui tail turducken est pancetta laborum cupidatat kielbasa tenderloin meatloaf. Est ball tip officia, aliquip shankle cillum meatloaf. Short loin pancetta ea culpa pariatur ut kevin." }
+    ]
+  },
+  {
+    "type":"graphic",
+    "caption":"winter has come",
+    "img": "assets/img/winter.jpg"
+  },
+  {
+    "type":"section",
+    "paragraphs": [
+       { "content": "Bacon ipsum dolor sit amet strip steak pork chop eu nostrud sint esse ham hock. Anim duis jerky, dolore sed do tongue id. Tenderloin tail frankfurter doner bresaola et bacon aliqua officia ut ribeye ut. Consectetur sirloin shank ball tip, drumstick quis labore t-bone eu pariatur rump. Cillum dolore pork, quis drumstick qui tail turducken est pancetta laborum cupidatat kielbasa tenderloin meatloaf. Est ball tip officia, aliquip shankle cillum meatloaf. Short loin pancetta ea culpa pariatur ut kevin." },
+       { "content": "Ullamco nulla proident excepteur, adipisicing in turkey shoulder enim meatloaf dolore consequat strip steak corned beef. Shoulder velit sirloin turducken dolore tongue kevin porchetta meatloaf boudin magna nulla tri-tip bresaola venison. Aute sirloin capicola, swine ut qui shank bresaola in boudin anim aliquip sausage commodo. Qui short ribs velit frankfurter in commodo. Kevin porchetta enim laboris ut fatback rump. Ut esse shoulder, dolore quis sirloin tempor filet mignon laboris anim pork belly beef spare ribs."},
+       { "content": "Bacon ipsum dolor sit amet strip steak pork chop eu nostrud sint esse ham hock. Anim duis jerky, dolore sed do tongue id. Tenderloin tail frankfurter doner bresaola et bacon aliqua officia ut ribeye ut. Consectetur sirloin shank ball tip, drumstick quis labore t-bone eu pariatur rump. Cillum dolore pork, quis drumstick qui tail turducken est pancetta laborum cupidatat kielbasa tenderloin meatloaf. Est ball tip officia, aliquip shankle cillum meatloaf. Short loin pancetta ea culpa pariatur ut kevin." }
+    ]
+  }
+];
+
+
+$(document).ready(function () {
+
+  var article = $('.article-container');
+
+  var templates = {
+    mast: _.template($('#banner-template').html()),
+    section: _.template($('#section-template').html()),
+    blurb: _.template($('#blurb-template').html()),
+    hiddenSection: _.template($('#hidden-section-template').html()),
+    iframe: _.template($('#iframe-template').html()),
+    graphic: _.template($('#graphic-template').html()),
+  };
+
+  _.each(components, function(component) {
+    if (component.type !== 'blurb') article.append(templates[component.type](component));
+    else {
+      var selector = $('section.normal').last().children()[component.paragraphNum];
+      $(selector).find('div.info').html(templates.blurb(component));
+    }
+  });
+  // Keep this at the very end of this script
+  $('.substory').on('click', function(e) {
+    $(this).next().toggleClass('hidden');
+    $(this).find('i').toggleClass('fa-rotate-180');
+  });
+});
---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http'),
+    express = require('express'),
+    port = 3000;
+
+
+var app = express();
+app.configure(function () {
+  app.use(express.json());
+  app.use(express.static(__dirname));
+});
+
+
+var httpServer = http.createServer(app).listen(port, function () {
+  console.log('Article listening on port ' + port);
+});
